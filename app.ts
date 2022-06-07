056906e4e7ca508d3b0faa7a0643080684e680bd
diasjames19 <50461532+diasjames19@users.noreply.github.com>
2022-06-07 12:27:36 -0300

    Os très desafio realizado

---
+++ app.ts
@@ -0,0 +1,56 @@
+import { NumberLiteralType } from "typescript";
+
+/* O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+
+campoSaldo.innerHTML = 0
+
+function somarAoSaldo(soma) {
+    campoSaldo.innerHTML += soma;
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(soma.value);
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */ 
+    let botaoAtualizar = document.getElementById('atualizar-saldo');
+    let botaoLimpar = document.getElementById('limpar-saldo');
+    let soma = document.getElementById('soma')  as HTMLInputElement;
+    let campoSaldo = document.getElementById('campo-saldo') as HTMLInputElement;
+    var  saldo = 0;
+    function Depositar(valor:number){
+        saldo = saldo + valor;
+        return saldo;
+
+        
+    }
+    if(botaoAtualizar){
+        botaoAtualizar.addEventListener('click',() => {
+            console.log(Depositar(Number(soma.value)));
+            campoSaldo.innerHTML = String(saldo);
+        });
+
+    }
+    if(botaoLimpar){
+        botaoLimpar.addEventListener('click', () =>{
+            campoSaldo.innerText = '';
+        })
+    }
